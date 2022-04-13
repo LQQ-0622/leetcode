16b5a82841ad876078336e5b5e154bf6e7a9d225
LQQ-0622 <luqi@ebupt.com>
2022-04-13 14:32:21 +0800

    2022/4/12-34

--- hot100/34在排序数组中查找元素的第一个和最后一个位置!.py
+++ hot100/34在排序数组中查找元素的第一个和最后一个位置!.py
@@ -3,20 +3,25 @@ class Solution:
         n = len(nums)
         if n == 0 or target < nums[0] or target > nums[n - 1]:
             return [-1, -1]
-        left, right = -1, -1
 
-        def binarySearch(nums, target, symbol):
+        def binarySearch(nums, target, lower):
             n = len(nums)
             l, r = 0, n - 1
+            ans = n
             while l <= r:
                 mid = (l + r) // 2
-                if nums[mid] == target:
-                    left = right = mid
-                    break
-                if nums[mid] < target:
-                    l = mid + 1
-                else:
+                if nums[mid] > target or (lower and nums[mid] >= target):
                     r = mid - 1
+                    ans = mid
+                else:
+                    l = mid + 1
+            return ans
+
+        left = binarySearch(nums, target, True)
+        right = binarySearch(nums, target, False) - 1
+        if left <= right and nums[left] == target and nums[right] == target:
+            return [left, right]
+        return [-1, -1]
 
     def searchRange1(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
@@ -43,6 +48,6 @@ class Solution:
         return [left, right]
 
 
-input = [1, 2, 2, 2, 4]
-result = Solution().searchRange(input, 2)
+input = [1]
+result = Solution().searchRange(input, 1)
 print(result)
